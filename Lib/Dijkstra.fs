c7ea131da87f448362dcdfb6d54a8f316b054395
KingVanti <kingvanti650@gmail.com>
2021-12-18 12:33:42 +0100

    Refactor for readability

--- Lib/Dijkstra.fs
+++ Lib/Dijkstra.fs
@@ -4,44 +4,50 @@ module ComradeVanti.FDijkstra.Dijkstra
 open System
 open Microsoft.FSharp.Collections
 
-let solve start goal vertices neighbors distance =
+let solveAll start vertices neighbors distance =
 
-    let mutable distances =
+    let distances =
         vertices
         |> List.map (fun v -> v, Int32.MaxValue)
         |> Map.ofList
         |> Map.add start 0
 
-    let mutable prev = vertices |> List.map (fun v -> v, None) |> Map.ofList
+    let prev = vertices |> List.map (fun v -> v, None) |> Map.ofList
 
     let findMinDistVertex =
         Set.toList
         >> List.sortBy (fun u -> distances.[u])
         >> List.head
 
-    let rec searchUntilDone unvisited =
+    let rec searchUntilDone unvisited (distances, prev) =
         if unvisited |> Set.isEmpty then
-            ()
+            prev
         else
             let u = unvisited |> findMinDistVertex
+
+            let checkNeighbor (distances, prev) v =
+                let alt = (distances |> Map.find u) + distance u v
+
+                if alt < (distances |> Map.find v) then
+                    distances |> Map.add v alt, prev |> Map.add v (Some u)
+                else
+                    distances, prev
 
             u
             |> neighbors
             |> List.filter (fun v -> unvisited |> Seq.contains v)
-            |> List.iter
-                (fun v ->
-                    let alt = distances.[u] + distance u v
+            |> List.fold checkNeighbor (distances, prev)
+            |> (searchUntilDone (unvisited |> Set.remove u))
 
-                    if alt < distances.[v] then
-                        distances <- distances |> Map.add v alt
-                        prev <- prev |> Map.add v (Some u))
+    searchUntilDone (vertices |> Set.ofList) (distances, prev)
 
-            searchUntilDone (unvisited |> Set.remove u)
+let solve start goal vertices neighbors distance =
+
+    let prev = solveAll start vertices neighbors distance
 
     let rec makePath current =
         match prev.[current] with
         | Some prev -> (makePath prev) @ [ current ]
         | None -> [ current ]
 
-    searchUntilDone (vertices |> Set.ofList)
     makePath goal
